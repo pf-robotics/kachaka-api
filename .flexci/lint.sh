57ca24d96cacd5189c1940bd09d0c963a5310726
nozaki-pfr <nozaki@pfrobotics.jp>
2023-08-16 09:25:40 +0900

    Add lint on github actions (#13)
    lintをgithub actionsでも実行するようにします

--- .flexci/lint.sh
+++ .flexci/lint.sh
@@ -5,5 +5,5 @@ set -eu
 docker run --rm \
     -v "$(pwd)":"$(pwd)" \
     --workdir="$(pwd)" \
-    asia-northeast1-docker.pkg.dev/pfr-flexci/tmp/kachaka-api.lint:KEEP-20230804 \
+    asia-northeast1-docker.pkg.dev/pfr-flexci/tmp/kachaka-api.lint:KEEP-20230815 \
     bash ./tools/lint/local_run.sh
