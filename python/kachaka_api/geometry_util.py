0324e3771e996345c886e73211b9e3b68933be33
ksyundo-pfr <ksyundo@pfrobotics.jp>
2023-08-17 11:04:03 +0900

    utility to calculate the yaw from quaternion (#19)
    quaternionをz軸周りの回転角度として扱うための関数です。odometryを扱うときにあると便利なのでutilに置きます。

--- python/kachaka_api/geometry_util.py
+++ python/kachaka_api/geometry_util.py
@@ -1,9 +1,17 @@
-from math import cos, sin
+from math import atan2, cos, pi, sin
 from typing import Tuple
 
 import numpy as np
 
-from .generated.kachaka_api_pb2 import Map, Pose
+from .generated.kachaka_api_pb2 import Map, Pose, Quaternion
+
+
+def calculate_yaw_from_quaternion(q: Quaternion) -> float:
+    # カチャカの姿勢はz軸周りの回転のみ含むため、他の軸は無視して算出する
+    yaw = atan2(q.z, q.w) * 2
+    if abs(yaw) > pi:
+        yaw -= np.sign(yaw) * 2 * pi
+    return yaw
 
 
 def calculate_2d_transform_matrix(
