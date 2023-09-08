b792ecde674d42f31d5f54fd8e206690d03fa82e
Watanabe Takafumi <watanabe@pfrobotics.jp>
2023-09-08 10:35:00 +0900

    open browser on run dev

--- web/demos/kachaka_api_web_sample/vite.config.ts
+++ web/demos/kachaka_api_web_sample/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: true,
+    port: 5173,
+  }
 })
