120d9f5e9cd826a0b745943bffea7df8f076b0ef
watanabe-pfr <watanabe@pfrobotics.jp>
2023-09-08 10:19:38 +0900

    Update web/demos/kachaka_api_web_sample/src/App.tsx
    Co-authored-by: Kenichi Hidai <hidai@pfrobotics.jp>
    Signed-off-by: watanabe-pfr <watanabe@pfrobotics.jp>

--- web/demos/kachaka_api_web_sample/src/App.tsx
+++ web/demos/kachaka_api_web_sample/src/App.tsx
@@ -78,9 +78,7 @@ function App(): JSX.Element {
               <Grid item xs={12} md={12} lg={12} sx={{ p: 4 }}>
                 <Fab
                   color="primary"
-                  onClick={() => {
-                    handleAddPanel()
-                  }}
+                  onClick={handleAddPanel}
                 >
                   <AddIcon />
                 </Fab>
