fd779ba4d6e7b34ea0db99e26cd1857221dbcfbc
Koji Terada <terakoji@pfrobotics.jp>
2023-09-08 15:02:46 +0900

    Use ROS2 navigation2 via ros2bridge. (#39)
    このPRは2.1リリース後にマージする必要があります。
    
    navigation2をbridge経由で動かすために、以下を行いました。
    
    - tfの適正化
    - navigationの設定ファイルの追加

--- .flexci/lint.sh
+++ .flexci/lint.sh
@@ -5,5 +5,11 @@ set -eu
 docker run --rm \
     -v "$(pwd)":"$(pwd)" \
     --workdir="$(pwd)" \
-    asia-northeast1-docker.pkg.dev/pfr-flexci/tmp/kachaka-api.lint:KEEP-20230824 \
-    bash ./tools/lint/local_run.sh
+    asia-northeast1-docker.pkg.dev/pfr-flexci/tmp/kachaka-api.lint:KEEP-20230828 \
+    bash << EOF
+        python3 -m venv /tmp/env
+        source /tmp/env/bin/activate
+        pip3 install -r tools/lint/requirements.txt
+        pip3 install -e .
+        ./tools/lint/local_run.sh
+EOF
---
+++ ros2/kachaka_grpc_ros2_bridge/src/dynamic_tf_bridge.cpp
@@ -0,0 +1,75 @@
+//  Copyright 2023 Preferred Robotics, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//     http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "dynamic_tf_bridge.hpp"
+
+#include <rclcpp/rclcpp.hpp>
+#include <tf2_msgs/msg/tf_message.hpp>
+#include <tf2_ros/qos.hpp>
+
+#include "converter/ros_header.hpp"
+#include "kachaka-api.grpc.pb.h"
+
+namespace {
+bool ConvertGrpcTfToRosTf(
+    const kachaka_api::GetDynamicTransformResponse& grpc_msg,
+    tf2_msgs::msg::TFMessage* msg) {
+  msg->transforms.clear();
+  msg->transforms.reserve(grpc_msg.transforms_size());
+  for (const auto& transform_grpc : grpc_msg.transforms()) {
+    geometry_msgs::msg::TransformStamped transform_ros;
+    kachaka::grpc_ros2_bridge::converter::ConvertGrpcHeaderToRos2Header(
+        transform_grpc.header(), &(transform_ros.header));
+
+    transform_ros.child_frame_id = transform_grpc.child_frame_id();
+    transform_ros.transform.translation.x = transform_grpc.translation().x();
+    transform_ros.transform.translation.y = transform_grpc.translation().y();
+    transform_ros.transform.translation.z = transform_grpc.translation().z();
+    transform_ros.transform.rotation.x = transform_grpc.rotation().x();
+    transform_ros.transform.rotation.y = transform_grpc.rotation().y();
+    transform_ros.transform.rotation.z = transform_grpc.rotation().z();
+    transform_ros.transform.rotation.w = transform_grpc.rotation().w();
+
+    msg->transforms.push_back(transform_ros);
+  }
+  return true;
+}
+
+}  // namespace
+
+namespace kachaka::grpc_ros2_bridge {
+
+TfStreamClient::TfStreamClient(
+    std::shared_ptr<kachaka_api::KachakaApi::Stub> stub, rclcpp::Node* node)
+    : stub_(stub),
+      node_(node),
+      publisher_(node->create_publisher<tf2_msgs::msg::TFMessage>(
+          "/tf", tf2_ros::DynamicBroadcasterQoS(5))) {}
+
+void TfStreamClient::ReadStream() {
+  grpc::ClientContext context;
+  kachaka_api::EmptyRequest request;
+
+  std::unique_ptr<grpc::ClientReader<kachaka_api::GetDynamicTransformResponse>>
+      reader(stub_->GetDynamicTransform(&context, request));
+
+  kachaka_api::GetDynamicTransformResponse response;
+
+  while (reader->Read(&response)) {
+    tf2_msgs::msg::TFMessage msg;
+    ConvertGrpcTfToRosTf(response, &msg);
+    publisher_->publish(msg);
+  }
+  RCLCPP_INFO(node_->get_logger(), "dynamic tf server is stopped.");
+}
+
+}  // namespace kachaka::grpc_ros2_bridge
