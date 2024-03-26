aa6eb67aaa5f2fb1badb6b94b4c19861e0420ec4
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2024-03-26 09:34:36 +0900

    rename

--- ros2/demos/kachaka_nav2_bringup/launch/navigation_launch.py
+++ ros2/demos/kachaka_nav2_bringup/launch/navigation_launch.py
@@ -30,7 +30,7 @@ from nav2_common.launch import RewrittenYaml
 
 def generate_launch_description():
     # Get the launch directory
-    bringup_dir = get_package_share_directory("kachaka_bringup")
+    bringup_dir = get_package_share_directory("kachaka_nav2_bringup")
 
     namespace = LaunchConfiguration("namespace")
     use_sim_time = LaunchConfiguration("use_sim_time")
