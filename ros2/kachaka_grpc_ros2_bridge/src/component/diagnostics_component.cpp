6e8746f1c1a2ca35ae30e7d6b9888303eab44547
T.Yoshikai <yoshikai@qibitech.com>
2024-08-23 20:51:46 +0900

    refactoring for passing lint checks

--- ros2/kachaka_grpc_ros2_bridge/src/component/diagnostics_component.cpp
+++ ros2/kachaka_grpc_ros2_bridge/src/component/diagnostics_component.cpp
@@ -12,13 +12,13 @@
 
 #include <memory>
 
-#include <grpcpp/grpcpp.h>
-#include <rclcpp/rclcpp.hpp>
-#include <rclcpp_components/register_node_macro.hpp>
 #include <diagnostic_msgs/msg/diagnostic_array.hpp>
 #include <diagnostic_msgs/msg/diagnostic_status.hpp>
 #include <diagnostic_msgs/msg/key_value.hpp>
+#include <grpcpp/grpcpp.h>
 #include <nlohmann/json.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp_components/register_node_macro.hpp>
 
 #include "../converter/ros_header.hpp"
 #include "../grpc_bridge.hpp"
@@ -32,14 +32,13 @@ using json = nlohmann::json;
 class DiagnosticsComponent : public rclcpp::Node {
  public:
   explicit DiagnosticsComponent(const rclcpp::NodeOptions& options)
-      : Node("diagnostic", options) {
+      : Node("diagnostics", options) {
     stub_ = GetSharedStub(declare_parameter("server_uri", ""));
 
     rclcpp::SensorDataQoS qos;
     using namespace std::placeholders;
-    get_robot_error_code_json_bridge_ = std::make_unique<
-        GrpcBridge<kachaka_api::EmptyRequest,
-                   kachaka_api::GetRobotErrorCodeJsonResponse>>(
+    get_robot_error_code_json_bridge_ = std::make_unique<GrpcBridge<
+        kachaka_api::EmptyRequest, kachaka_api::GetRobotErrorCodeJsonResponse>>(
         this, std::bind(&kachaka_api::KachakaApi::Stub::GetRobotErrorCodeJson,
                         *stub_, _1, _2, _3));
     kachaka_api::EmptyRequest request;
@@ -48,61 +47,64 @@ class DiagnosticsComponent : public rclcpp::Node {
     json response_dictionary;
     response_dictionary = json::parse(response.json());
     for (auto& element : response_dictionary) {
-        auto error_code = std::to_string(element[this->CODE].get<int>());
-        robot_error_dictionary_[error_code] = element;
+      auto error_code = std::to_string(element[this->CODE].get<int>());
+      robot_error_dictionary_[error_code] = element;
     }
 
-    diagnostic_bridge_ = std::make_unique<
-        Ros2TopicBridge<kachaka_api::GetErrorResponse, diagnostic_msgs::msg::DiagnosticArray>>(
+    diagnostic_bridge_ = std::make_unique<Ros2TopicBridge<
+        kachaka_api::GetErrorResponse, diagnostic_msgs::msg::DiagnosticArray>>(
         this,
-        std::bind(&kachaka_api::KachakaApi::Stub::GetError, *stub_, _1, _2,
-                  _3),
-        "~/diagnostics", qos);
-    diagnostic_bridge_->SetConverter([&, this](const kachaka_api::GetErrorResponse& grpc_msg,
-                                        diagnostic_msgs::msg::DiagnosticArray* ros2_msg) {
-      ros2_msg->header.stamp = this->now();
-      ros2_msg->header.frame_id = "";
+        std::bind(&kachaka_api::KachakaApi::Stub::GetError, *stub_, _1, _2, _3),
+        "~/error_status", qos);
+    diagnostic_bridge_->SetConverter(
+        [&, this](const kachaka_api::GetErrorResponse& grpc_msg,
+                  diagnostic_msgs::msg::DiagnosticArray* ros2_msg) {
+          ros2_msg->header.stamp = this->now();
+          ros2_msg->header.frame_id = "";
 
-      const auto& error_codes = grpc_msg.error_codes();
-      for (auto& ec : error_codes) {
-        auto ec_str = std::to_string(ec);
-        if (this->robot_error_dictionary_.contains(ec_str)) {
-          const auto& error_info = this->robot_error_dictionary_[ec_str];
-          diagnostic_msgs::msg::DiagnosticStatus diag;
-          if (error_info[this->ERROR_TYPE] == this->ERROR_TYPE_BUG ||
-            error_info[this->ERROR_TYPE] == this->ERROR_TYPE_IGNORE) {
-              diag.level = diagnostic_msgs::msg::DiagnosticStatus::OK;
-          } else if (error_info[this->ERROR_TYPE] == this->ERROR_TYPE_WARN) {
-            diag.level = diagnostic_msgs::msg::DiagnosticStatus::WARN;
-          } else {
-            // ERROR_TYPE_ERROR, ERROR_TYPE_FATAL ERROR_TYPE_CALL_FOR_SUPPORT
-            diag.level = diagnostic_msgs::msg::DiagnosticStatus::ERROR;
+          const auto& error_codes = grpc_msg.error_codes();
+          for (auto& ec : error_codes) {
+            auto ec_str = std::to_string(ec);
+            if (this->robot_error_dictionary_.contains(ec_str)) {
+              const auto& error_info = this->robot_error_dictionary_[ec_str];
+              diagnostic_msgs::msg::DiagnosticStatus diag;
+              if (error_info[this->ERROR_TYPE] == this->ERROR_TYPE_BUG ||
+                  error_info[this->ERROR_TYPE] == this->ERROR_TYPE_IGNORE) {
+                diag.level = diagnostic_msgs::msg::DiagnosticStatus::OK;
+              } else if (error_info[this->ERROR_TYPE] ==
+                         this->ERROR_TYPE_WARN) {
+                diag.level = diagnostic_msgs::msg::DiagnosticStatus::WARN;
+              } else {
+                // ERROR_TYPE_ERROR, ERROR_TYPE_FATAL
+                // ERROR_TYPE_CALL_FOR_SUPPORT
+                diag.level = diagnostic_msgs::msg::DiagnosticStatus::ERROR;
+              }
+              diag.name = error_info[this->TITLE_EN].get<std::string>();
+              diag.message = error_info[this->DESC_EN].get<std::string>();
+              diagnostic_msgs::msg::KeyValue code, title, description,
+                  error_type, ref_url;
+              code.key = this->CODE;
+              code.value = std::to_string(error_info[this->CODE].get<int>());
+              title.key = this->TITLE;
+              title.value = error_info[this->TITLE].get<std::string>();
+              description.key = this->DESC;
+              description.value = error_info[this->DESC].get<std::string>();
+              error_type.key = this->ERROR_TYPE;
+              error_type.value =
+                  error_info[this->ERROR_TYPE].get<std::string>();
+              ref_url.key = this->REF_URL;
+              ref_url.value = error_info[this->REF_URL].get<std::string>();
+              diag.values.emplace_back(code);
+              diag.values.emplace_back(title);
+              diag.values.emplace_back(description);
+              diag.values.emplace_back(error_type);
+              diag.values.emplace_back(ref_url);
+              ros2_msg->status.emplace_back(diag);
+            }
           }
-          diag.name = error_info[this->TITLE_EN].get<std::string>();
-          diag.message = error_info[this->DESC_EN].get<std::string>();
-          diagnostic_msgs::msg::KeyValue code, title, description, error_type, ref_url;
-          code.key = this->CODE;
-          code.value = error_info[this->CODE].get<std::string>();
-          title.key = this->TITLE;
-          title.value = error_info[this->TITLE].get<std::string>();
-          description.key = this->DESC;
-          description.value = error_info[this->DESC].get<std::string>();
-          error_type.key = this->ERROR_TYPE;
-          error_type.value = error_info[this->ERROR_TYPE].get<std::string>();
-          ref_url.key = this->REF_URL;
-          ref_url.value = error_info[this->REF_URL].get<std::string>();
 
-          diag.values.emplace_back(code);
-          diag.values.emplace_back(title);
-          diag.values.emplace_back(description);
-          diag.values.emplace_back(error_type);
-          diag.values.emplace_back(ref_url);
-          ros2_msg->status.emplace_back(diag);
-        }
-      }
-
-      return true;
-    });
+          return true;
+        });
 
     diagnostic_bridge_->StartAsync();
   }
@@ -127,8 +129,8 @@ class DiagnosticsComponent : public rclcpp::Node {
   const std::string ERROR_TYPE_CALL_FOR_SUPPORT = "CallForSupport";
   json robot_error_dictionary_;
   std::shared_ptr<kachaka_api::KachakaApi::Stub> stub_{nullptr};
-  std::unique_ptr<
-      Ros2TopicBridge<kachaka_api::GetErrorResponse, diagnostic_msgs::msg::DiagnosticArray>>
+  std::unique_ptr<Ros2TopicBridge<kachaka_api::GetErrorResponse,
+                                  diagnostic_msgs::msg::DiagnosticArray>>
       diagnostic_bridge_{nullptr};
   std::unique_ptr<GrpcBridge<kachaka_api::EmptyRequest,
                              kachaka_api::GetRobotErrorCodeJsonResponse>>
