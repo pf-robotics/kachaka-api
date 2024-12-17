476b44d7bca7a8d4c9ba418c5148ed02dd52f553
watanabe-pfr <watanabe@pfrobotics.jp>
2024-12-17 09:27:01 +0900

    update_kachaka_api_generated.sh を更新
    Co-authored-by: Kenichi Hidai <hidai@pfrobotics.jp>
    Signed-off-by: watanabe-pfr <watanabe@pfrobotics.jp>

--- tools/update_kachaka_api_generated.sh
+++ tools/update_kachaka_api_generated.sh
@@ -16,7 +16,7 @@ popd || exit 1
 
 # run docker image
 REPO_TOP_DIR="${SCRIPT_DIR}/.."
-cd "${SCRIPT_DIR}/.." || exit 1
+cd "${REPO_TOP_DIR}" || exit 1
 docker run --rm \
     -v "${REPO_TOP_DIR}/protos:/protos" \
     -v "${REPO_TOP_DIR}/python/kachaka_api/generated:/generated" \
