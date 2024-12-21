0c474f910a541f8c545b2a7248fec65d569e9471
MrKeith99 <kvalentincardenas@hotmail.com>
2024-12-21 20:37:14 +0900

    [FEAT] add kachaka name as an option

--- tools/ros2_bridge/start_bridge.sh
+++ tools/ros2_bridge/start_bridge.sh
@@ -6,13 +6,21 @@ DOCKER_COMPOSE_DIR="$(cd "$(dirname "$0")" && pwd)"
 cd "${DOCKER_COMPOSE_DIR}"
 
 usage() {
-    echo "Usage: $0 KACHAKA_IP_ADRESS [Option]"
+    echo "Usage: $0 KACHAKA_IP_ADRESS [KACHAKA_NAME] [Option]"
     echo "  -d    daemonize"
     exit 1
 }
 
 if [[ $# -lt 1 ]]; then
     usage
+fi
+
+if [[ $# -lt 2 ]]; then
+    NAMESPACE="kachaka"
+    FRAME_PREFIX='/'
+else
+    NAMESPACE=$2
+    FRAME_PREFIX=$2"/"
 fi
 
 USER_ID="$(id -u)"
@@ -25,7 +33,7 @@ export GROUP_ID
 KACHAKA_IP=$1
 
 if command -v docker-compose; then
-    API_GRPC_BRIDGE_SERVER_URI="${KACHAKA_IP}:${GRPC_PORT}" docker-compose up "${@:2}" ros2_bridge 
+    API_GRPC_BRIDGE_SERVER_URI="${KACHAKA_IP}:${GRPC_PORT}" NAMESPACE="${NAMESPACE}" FRAME_PREFIX="${FRAME_PREFIX}" docker-compose up "${@:4}" ros2_bridge 
 else
-    API_GRPC_BRIDGE_SERVER_URI="${KACHAKA_IP}:${GRPC_PORT}" docker compose up "${@:2}" ros2_bridge
+    API_GRPC_BRIDGE_SERVER_URI="${KACHAKA_IP}:${GRPC_PORT}" NAMESPACE="${NAMESPACE}" FRAME_PREFIX="${FRAME_PREFIX}" docker compose up "${@:4}" ros2_bridge
 fi
