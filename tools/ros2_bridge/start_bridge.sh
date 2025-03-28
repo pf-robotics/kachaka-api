133686abbcd63b44c4af3f7e248f0d7bcd989319
MrKeith99 <kvalentincardenas@hotmail.com>
2025-03-28 20:51:33 +0900

    [UPD] allow select to publish robot

--- tools/ros2_bridge/start_bridge.sh
+++ tools/ros2_bridge/start_bridge.sh
@@ -6,8 +6,10 @@ DOCKER_COMPOSE_DIR="$(cd "$(dirname "$0")" && pwd)"
 cd "${DOCKER_COMPOSE_DIR}"
 
 usage() {
-    echo "Usage: $0 KACHAKA_IP_ADRESS [KACHAKA_NAME] [Option]"
-    echo "  -d    daemonize"
+    echo "Usage: $0 KACHAKA_IP_ADRESS [KACHAKA_NAME] [KACHAKA_STATE] [Option]"
+    echo "  KACHAKA_NAME:  kachaka"
+    echo "  KACHAKA_STATE: yes/no"
+    echo "  -d             daemonize"
     exit 1
 }
 
@@ -23,6 +25,18 @@ else
     FRAME_PREFIX=$2"/"
 fi
 
+if [[ $# -lt 3 ]]; then
+    KACHAKA_STATE="yes"
+else
+    KACHAKA_STATE=$3
+fi
+
+if [[ "${KACHAKA_STATE}" == "yes" ]]; then
+    LAUNCHER_CMD="bridge"
+else
+    LAUNCHER_CMD="bridge_no_state"
+fi
+
 USER_ID="$(id -u)"
 GROUP_ID="$(id -g)"
 GRPC_PORT=26400
@@ -33,7 +47,7 @@ export GROUP_ID
 KACHAKA_IP=$1
 
 if command -v docker-compose; then
-    API_GRPC_BRIDGE_SERVER_URI="${KACHAKA_IP}:${GRPC_PORT}" NAMESPACE="${NAMESPACE}" FRAME_PREFIX="${FRAME_PREFIX}" docker-compose up "${@:3}" ros2_bridge
+    API_GRPC_BRIDGE_SERVER_URI="${KACHAKA_IP}:${GRPC_PORT}" NAMESPACE="${NAMESPACE}" FRAME_PREFIX="${FRAME_PREFIX}" LAUNCHER_CMD="${LAUNCHER_CMD}" docker-compose up "${@:4}" ros2_bridge
 else
-    API_GRPC_BRIDGE_SERVER_URI="${KACHAKA_IP}:${GRPC_PORT}" NAMESPACE="${NAMESPACE}" FRAME_PREFIX="${FRAME_PREFIX}" docker compose up "${@:3}" ros2_bridge
+    API_GRPC_BRIDGE_SERVER_URI="${KACHAKA_IP}:${GRPC_PORT}" NAMESPACE="${NAMESPACE}" FRAME_PREFIX="${FRAME_PREFIX}" LAUNCHER_CMD="${LAUNCHER_CMD}" docker compose up "${@:4}" ros2_bridge
 fi
