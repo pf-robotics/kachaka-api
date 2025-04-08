07275197078398f263f871ca79104ef733bc3f45
Watanabe Takafumi <watanabe@pfrobotics.jp>
2025-04-08 22:52:46 +0900

    use common pyproject.toml to generate code from proto

--- tools/gen_proto/python_entrypoint.sh
+++ tools/gen_proto/python_entrypoint.sh
@@ -5,15 +5,14 @@ set -eu
 export UV_CACHE_DIR=/tmp/.cache/uv
 export UV_PYTHON_INSTALL_DIR=/tmp/.local/share/uv
 
-pushd /app
-uv sync
-
-uv run python -m grpc_tools.protoc \
-    -I/protos \
-    --python_out="/generated" \
-    --pyi_out="/generated" \
-    --grpc_python_out="/generated" \
-    /protos/kachaka-api.proto
+uv run \
+    --with "grpcio-tools==1.66.1" \
+    python -m grpc_tools.protoc \
+      -I/protos \
+      --python_out="/generated" \
+      --pyi_out="/generated" \
+      --grpc_python_out="/generated" \
+      /protos/kachaka-api.proto
 
 sed "/generated"/kachaka_api_pb2_grpc.py -i \
     -e "s/import kachaka_api_pb2/from . import kachaka_api_pb2/" \
