902564fde2b89e54b9c1a5dae301c72de3c193be
Kotaro Nagahama <nagahama@pfrobotics.jp>
2026-08-31 11:42:45 +0900

    Add DepartFromCharger command

--- python/kachaka_api/base.py
+++ python/kachaka_api/base.py
@@ -511,6 +511,24 @@ class KachakaApiClientBase:
             title=title,
         )
 
+    def depart_from_charger(
+        self,
+        *,
+        wait_for_completion: bool = True,
+        cancel_all: bool = True,
+        tts_on_success: str = "",
+        title: str = "",
+    ) -> pb2.Result:
+        return self.start_command(
+            pb2.Command(
+                depart_from_charger_command=pb2.DepartFromChargerCommand()
+            ),
+            wait_for_completion=wait_for_completion,
+            cancel_all=cancel_all,
+            tts_on_success=tts_on_success,
+            title=title,
+        )
+
     def cancel_command(self) -> tuple[pb2.Result, pb2.Command]:
         request = pb2.EmptyRequest()
         response: pb2.CancelCommandResponse = self.stub.CancelCommand(request)
